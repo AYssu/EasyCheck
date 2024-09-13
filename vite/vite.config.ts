10a89d620f277b462ed8c364ecfd164c9b8a29c5
AYssu <2997036064@qq.com>
2024-09-13 13:29:55 +0800

    创建vite项目并配置相关参数

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src'), // 路径别名
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  }
 })
