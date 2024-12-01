1d939a018bee1396b52521c297707d25f991173f
AYssu <2997036064@qq.com>
2024-12-01 23:37:47 +0800

    修改OPENAPI方法 完成公告获取

--- springboot/src/main/java/com/easyverify/springboot/dto/OpenAPIDTO.java
+++ springboot/src/main/java/com/easyverify/springboot/dto/OpenAPIDTO.java
@@ -1,8 +1,14 @@
 package com.easyverify.springboot.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class OpenAPIDTO {
-    private Integer pid; // 程序ID
+    @NotBlank(message = "前端配置异常")
+    private String pid; // 程序ID
+    @NotBlank(message = "前端配置异常")
+    private String time;
+    @NotBlank(message = "前端配置异常")
+    private String sign;
 }
---
+++ springboot/src/test/java/com/easyverify/springboot/SutilsTests.java
@@ -0,0 +1,27 @@
+package com.easyverify.springboot;
+
+import com.easyverify.springboot.utils.Sutils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SutilsTests {
+    @Test
+    public void base64(){
+        String str = "abc阿夜6哔";
+        Sutils.set_base64(""); // 留空默认
+        System.out.println(Sutils.base64_encode(str));
+        Sutils.set_base64("RbZ4y1KO0WahcfG8rQ7TBVUotNgmdXH/DMxv5zEY+esP6SFwipl3LuIC9A2nJqjk");
+        System.out.println(Sutils.base64_encode(str));
+        System.out.println(Sutils.base64_decode("MzEzMDMwMzAzMA=="));
+    }
+
+    @Test
+    public void hex(){
+        String str = "abc阿夜6哔";
+        System.out.println(Sutils.to_hex(str));
+        System.out.println(Sutils.hex_to_string("4D7A457A4D444D774D7A417A4D413D3D"));
+        System.out.println(Sutils.hex_to_string("4D7A457A4D444D774D7A417A4D413D3D"));
+        System.out.println(Sutils.hex_to_string("4D7A457A4D444D774D7A417A4D413D3D"));
+    }
+}
